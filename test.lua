29d224fec8cc3c6ede46c73118501dbb7ff6e390
TheChillFighter <108900073+TheChillFighter@users.noreply.github.com>
2025-12-11 03:19:38 -0500

    Update print statements for clarity and language

--- test.lua
+++ test.lua
@@ -1,13 +1,13 @@
 local number = 0
 Citizen.CreateThread(function()
-    print("Valla Loadlandım Abe Bak Çalışcam Birazdan!")
+    print("Done, will try soon!")
     while true do
         Citizen.Wait(math.random(1000,3000))
         number = number + 1
-        print("Hala Çalışıyom Abe Bak Sayam Sana "..number)
+        print("Let me count "..number)
     end
 end)
 
 RegisterCommand("123", function()
-    print("Chate 123 Yazam çalışam")
+    print("123")
 end)
